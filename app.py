0688cd38992c843d194d5eb9bd4d9029c78bbfb6
morgan8514 <morgangicheha4@gmail.com>
2019-10-26 14:39:37 +0300

    improved the download function

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 from flask import Flask,redirect,render_template,request,url_for
 from flask_sqlalchemy import SQLAlchemy
 from pytube import YouTube
-import urllib.request
+
 import os
 
 app = Flask(__name__)
-
-
-# importing db from download_info.py file
-
-
-
 # creatimg configs
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:morgan8514@127.0.0.1:5432/YD'
 # dialect+driver://username:password@host:port/database
@@ -27,7 +21,7 @@ def create():
 def home():
     if request.method=='POST':
         recieved_url= request.form['users_input_url']
-        print('imeingia')
+        print('url recieved')
         # python function to download video
         def download_yt(url):
             print('getting video...')
@@ -37,31 +31,15 @@ def home():
             streams=yt.streams.first()
             print('getting video title...')
             # print(yt.title)
-            print('downloadint video...')
+            print('downloading video...')
             yt.streams.download()
             print('#####Download complete#####')
-            
-        download_yt(recieved_url)
-        print("pgrogress")
-    
-        
-      
-        
-
+            # calling function to download
+        download_video = download_yt(recieved_url)
+           
         return render_template('home.html')
-# route to view videoes available for download
-# @app.route('/home/download' , methods=['GET','POST'])
-# def download_route():
 
-#     return render_template('view_streams_availabe.html')
-
-
-        
     return render_template('home.html')
-
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
